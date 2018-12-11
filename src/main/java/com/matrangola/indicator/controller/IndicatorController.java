36efeffd07c50fe809044bf1958a53d5e3ba9b1e
Geoff Matrangola <geoff@matrangola.com>
2018-12-11 14:11:58 -0500

    Lab 6

--- src/main/java/com/matrangola/indicator/controller/IndicatorController.java
+++ src/main/java/com/matrangola/indicator/controller/IndicatorController.java
@@ -1,10 +1,14 @@
 package com.matrangola.indicator.controller;
 
+import com.matrangola.indicator.data.model.Indicator;
 import com.matrangola.indicator.service.IndicatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/indicators")
@@ -20,4 +24,15 @@ public class IndicatorController {
     Double average(@RequestParam String indicatorCode) {
         return indicatorService.worldwideAverage(indicatorCode);
     }
+
+    @GetMapping("above")
+    public List<Indicator> aboveMin(@RequestParam String code, @RequestParam double min) {
+        return indicatorService.aboveMin(code, min);
+    }
+
+    @GetMapping("aboveAvg")
+    public List<Indicator> aboveAvg(@RequestParam String code) {
+        return indicatorService.aboveAverage(code);
+    }
+
 }
